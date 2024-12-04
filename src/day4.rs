f75c0d45e492dc26e177ff253acfd35a0ddc0dc2
D.J. Rideout <djrideoutgit@proton.me>
2024-12-04 23:02:43 +0000

    Day 4 better code reuse, probably good enough

--- src/day4.rs
+++ src/day4.rs
@@ -13,6 +13,20 @@ fn _get_char(pos: i32, curr_pos: i32, line_offset: i32, input: &str, line_len: i
     '.'
 }
 
+fn get_closures() -> Vec<Box<dyn Fn(&str, i32, i32, i32) -> char>> {
+    let get_char: Vec<Box<dyn Fn(&str, i32, i32, i32) -> char>> = vec![
+        Box::new(move |input, line_len, pos, distance| _get_char(pos + (-1) * distance, pos + (-1) * (distance - 1), 0, input, line_len)),                        // Left
+        Box::new(move |input, line_len, pos, distance| _get_char(pos + (1) * distance, pos + (1) * (distance - 1), 0, input, line_len)),                          // Right
+        Box::new(move |input, line_len, pos, distance| _get_char(pos + (-line_len - 1) * distance, pos + (-line_len - 1) * (distance - 1), -1, input, line_len)), // Up-left
+        Box::new(move |input, line_len, pos, distance| _get_char(pos + (-line_len) * distance, pos + (-line_len) * (distance - 1), -1, input, line_len)),         // Up
+        Box::new(move |input, line_len, pos, distance| _get_char(pos + (-line_len + 1) * distance, pos + (-line_len + 1) * (distance - 1), -1, input, line_len)), // Up-right
+        Box::new(move |input, line_len, pos, distance| _get_char(pos + (line_len + 1) * distance, pos + (line_len + 1) * (distance - 1), 1, input, line_len)),    // Down-right
+        Box::new(move |input, line_len, pos, distance| _get_char(pos + (line_len) * distance, pos + (line_len) * (distance - 1), 1, input, line_len)),            // Down
+        Box::new(move |input, line_len, pos, distance| _get_char(pos + (line_len - 1) * distance, pos + (line_len - 1) * (distance - 1), 1, input, line_len)),    // Down-left
+    ];
+    return get_char;
+}
+
 pub struct Input {
     input: String,
     line_len: i32
@@ -31,26 +45,7 @@ pub fn input_generator(in_lines: &str) -> Input {
 pub fn solve_part1(input_data: &Input) -> i32 {
     let line_len = input_data.line_len;
     let input = &input_data.input;
-    let offsets = vec![
-        -1,
-        1,
-        -line_len - 1,
-        -line_len,
-        -line_len + 1,
-        line_len + 1,
-        line_len,
-        line_len - 1
-    ];
-    let get_char: Vec<Box<dyn Fn(i32, i32) -> char>> = vec![
-        Box::new(|pos, distance| _get_char(pos + offsets[0] * distance, pos + offsets[0] * (distance - 1), 0, &input, line_len)),
-        Box::new(|pos, distance| _get_char(pos + offsets[1] * distance, pos + offsets[1] * (distance - 1), 0, &input, line_len)),
-        Box::new(|pos, distance| _get_char(pos + offsets[2] * distance, pos + offsets[2] * (distance - 1), -1, &input, line_len)),
-        Box::new(|pos, distance| _get_char(pos + offsets[3] * distance, pos + offsets[3] * (distance - 1), -1, &input, line_len)),
-        Box::new(|pos, distance| _get_char(pos + offsets[4] * distance, pos + offsets[4] * (distance - 1), -1, &input, line_len)),
-        Box::new(|pos, distance| _get_char(pos + offsets[5] * distance, pos + offsets[5] * (distance - 1), 1, &input, line_len)),
-        Box::new(|pos, distance| _get_char(pos + offsets[6] * distance, pos + offsets[6] * (distance - 1), 1, &input, line_len)),
-        Box::new(|pos, distance| _get_char(pos + offsets[7] * distance, pos + offsets[7] * (distance - 1), 1, &input, line_len)),
-    ];
+    let get_char = get_closures();
     let mut xmas_count = 0;
     let mut i = 0;
     for char in input.chars() {
@@ -58,11 +53,11 @@ pub fn solve_part1(input_data: &Input) -> i32 {
             i += 1;
             continue;
         }
-        for j in 0 .. offsets.len() {
+        for j in 0 .. 8 {
             let is_xmas =
-                get_char[j](i, 1) == 'M' &&
-                get_char[j](i, 2) == 'A' &&
-                get_char[j](i, 3) == 'S';
+                get_char[j](input, line_len, i, 1) == 'M' &&
+                get_char[j](input, line_len, i, 2) == 'A' &&
+                get_char[j](input, line_len, i, 3) == 'S';
             xmas_count += is_xmas as i32;
         }
         i += 1;
@@ -74,26 +69,7 @@ pub fn solve_part1(input_data: &Input) -> i32 {
 pub fn solve_part2(input_data: &Input) -> i32 {
     let line_len = input_data.line_len;
     let input = &input_data.input;
-    let offsets = vec![
-        -1,
-        1,
-        -line_len - 1,
-        -line_len,
-        -line_len + 1,
-        line_len + 1,
-        line_len,
-        line_len - 1
-    ];
-    let get_char: Vec<Box<dyn Fn(i32, i32) -> char>> = vec![
-        Box::new(|pos, distance| _get_char(pos + offsets[0] * distance, pos + offsets[0] * (distance - 1), 0, &input, line_len)),
-        Box::new(|pos, distance| _get_char(pos + offsets[1] * distance, pos + offsets[1] * (distance - 1), 0, &input, line_len)),
-        Box::new(|pos, distance| _get_char(pos + offsets[2] * distance, pos + offsets[2] * (distance - 1), -1, &input, line_len)),
-        Box::new(|pos, distance| _get_char(pos + offsets[3] * distance, pos + offsets[3] * (distance - 1), -1, &input, line_len)),
-        Box::new(|pos, distance| _get_char(pos + offsets[4] * distance, pos + offsets[4] * (distance - 1), -1, &input, line_len)),
-        Box::new(|pos, distance| _get_char(pos + offsets[5] * distance, pos + offsets[5] * (distance - 1), 1, &input, line_len)),
-        Box::new(|pos, distance| _get_char(pos + offsets[6] * distance, pos + offsets[6] * (distance - 1), 1, &input, line_len)),
-        Box::new(|pos, distance| _get_char(pos + offsets[7] * distance, pos + offsets[7] * (distance - 1), 1, &input, line_len)),
-    ];
+    let get_char = get_closures();
     let mut xmas_count = 0;
     let mut i = 0;
     for char in input.chars() {
@@ -102,11 +78,11 @@ pub fn solve_part2(input_data: &Input) -> i32 {
             continue;
         }
         let is_xmas_down =
-            get_char[2](i, 1) == 'M' && get_char[5](i, 1) == 'S' ||
-            get_char[5](i, 1) == 'M' && get_char[2](i, 1) == 'S';
+            get_char[2](input, line_len, i, 1) == 'M' && get_char[5](input, line_len, i, 1) == 'S' ||
+            get_char[5](input, line_len, i, 1) == 'M' && get_char[2](input, line_len, i, 1) == 'S';
         let is_xmas_up =
-            get_char[4](i, 1) == 'M' && get_char[7](i, 1) == 'S' ||
-            get_char[7](i, 1) == 'M' && get_char[4](i, 1) == 'S';
+            get_char[4](input, line_len, i, 1) == 'M' && get_char[7](input, line_len, i, 1) == 'S' ||
+            get_char[7](input, line_len, i, 1) == 'M' && get_char[4](input, line_len, i, 1) == 'S';
         xmas_count += (is_xmas_down && is_xmas_up) as i32;
         i += 1;
     }
