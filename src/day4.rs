c1d998d001a8285ca9917cf73049faa63d792789
D.J. Rideout <djrideoutgit@proton.me>
2024-12-04 19:42:28 +0000

    Day 4 some simplification

--- src/day4.rs
+++ src/day4.rs
@@ -1,29 +1,14 @@
-fn is_prev_line(pos: i32, curr_pos: i32, line_len: i32) -> bool {
+fn _get_char(pos: i32, curr_pos: i32, line_offset: i32, input: &str, line_len: i32) -> char {
+    if pos <= 0 || pos >= input.len() as i32 || curr_pos <= 0 || curr_pos >= input.len() as i32 {
+        return '.';
+    }
     let curr_row = curr_pos / line_len;
-    let prev_row_start = (curr_row - 1) * line_len;
-    let prev_row_end = prev_row_start + line_len - 1;
-    pos >= prev_row_start && pos <= prev_row_end
-}
-
-fn is_curr_line(pos: i32, curr_pos: i32, line_len: i32) -> bool {
-    let curr_row_start = (curr_pos / line_len) * line_len;
-    let curr_row_end = curr_row_start + line_len - 1;
-    pos >= curr_row_start && pos <= curr_row_end
-}
-
-fn is_next_line(pos: i32, curr_pos: i32, line_len: i32) -> bool {
-    let curr_row = curr_pos / line_len;
-    let next_row_start = (curr_row + 1) * line_len;
-    let next_row_end = next_row_start + line_len - 1;
-    pos >= next_row_start && pos <= next_row_end
-}
-
-fn is_inbounds(pos: i32, input: &str) -> bool {
-    pos >= 0 && pos < input.len() as i32
-}
-
-fn get_char(input: &str, pos: i32) -> char {
-    input.chars().nth(pos as usize).unwrap()
+    let other_row_start = (curr_row + line_offset) * line_len;
+    let other_row_end = other_row_start + line_len - 1;
+    if pos >= other_row_start && pos <= other_row_end {
+        return input.chars().nth(pos as usize).unwrap();
+    }
+    '.'
 }
 
 #[aoc(day4, part1)]
@@ -46,15 +31,15 @@ pub fn solve_part1(in_lines: &str) -> i32 {
         line_len,
         line_len - 1
     ];
-    let tests: Vec<Box<dyn Fn(i32, i32) -> bool>> = vec![
-        Box::new(|pos, curr_pos| is_inbounds(pos, &input) && is_inbounds(curr_pos, &input) && is_curr_line(pos, curr_pos, line_len)),
-        Box::new(|pos, curr_pos| is_inbounds(pos, &input) && is_inbounds(curr_pos, &input) && is_curr_line(pos, curr_pos, line_len)),
-        Box::new(|pos, curr_pos| is_inbounds(pos, &input) && is_inbounds(curr_pos, &input) && is_prev_line(pos, curr_pos, line_len)),
-        Box::new(|pos, curr_pos| is_inbounds(pos, &input) && is_inbounds(curr_pos, &input) && is_prev_line(pos, curr_pos, line_len)),
-        Box::new(|pos, curr_pos| is_inbounds(pos, &input) && is_inbounds(curr_pos, &input) && is_prev_line(pos, curr_pos, line_len)),
-        Box::new(|pos, curr_pos| is_inbounds(pos, &input) && is_inbounds(curr_pos, &input) && is_next_line(pos, curr_pos, line_len)),
-        Box::new(|pos, curr_pos| is_inbounds(pos, &input) && is_inbounds(curr_pos, &input) && is_next_line(pos, curr_pos, line_len)),
-        Box::new(|pos, curr_pos| is_inbounds(pos, &input) && is_inbounds(curr_pos, &input) && is_next_line(pos, curr_pos, line_len))
+    let get_char: Vec<Box<dyn Fn(i32, i32) -> char>> = vec![
+        Box::new(|pos, distance| _get_char(pos + offsets[0] * distance, pos + offsets[0] * (distance - 1), 0, &input, line_len)),
+        Box::new(|pos, distance| _get_char(pos + offsets[1] * distance, pos + offsets[1] * (distance - 1), 0, &input, line_len)),
+        Box::new(|pos, distance| _get_char(pos + offsets[2] * distance, pos + offsets[2] * (distance - 1), -1, &input, line_len)),
+        Box::new(|pos, distance| _get_char(pos + offsets[3] * distance, pos + offsets[3] * (distance - 1), -1, &input, line_len)),
+        Box::new(|pos, distance| _get_char(pos + offsets[4] * distance, pos + offsets[4] * (distance - 1), -1, &input, line_len)),
+        Box::new(|pos, distance| _get_char(pos + offsets[5] * distance, pos + offsets[5] * (distance - 1), 1, &input, line_len)),
+        Box::new(|pos, distance| _get_char(pos + offsets[6] * distance, pos + offsets[6] * (distance - 1), 1, &input, line_len)),
+        Box::new(|pos, distance| _get_char(pos + offsets[7] * distance, pos + offsets[7] * (distance - 1), 1, &input, line_len)),
     ];
     let mut xmas_count = 0;
     let mut i = 0;
@@ -65,9 +50,9 @@ pub fn solve_part1(in_lines: &str) -> i32 {
         }
         for j in 0 .. offsets.len() {
             let is_xmas =
-                tests[j](i + offsets[j] * 1, i + offsets[j] * 0) && get_char(&input, i + offsets[j] * 1) == 'M' &&
-                tests[j](i + offsets[j] * 2, i + offsets[j] * 1) && get_char(&input, i + offsets[j] * 2) == 'A' &&
-                tests[j](i + offsets[j] * 3, i + offsets[j] * 2) && get_char(&input, i + offsets[j] * 3) == 'S';
+                get_char[j](i, 1) == 'M' &&
+                get_char[j](i, 2) == 'A' &&
+                get_char[j](i, 3) == 'S';
             xmas_count += is_xmas as i32;
         }
         i += 1;
@@ -95,15 +80,15 @@ pub fn solve_part2(in_lines: &str) -> i32 {
         line_len,
         line_len - 1
     ];
-    let tests: Vec<Box<dyn Fn(i32, i32) -> bool>> = vec![
-        Box::new(|pos, curr_pos| is_inbounds(pos, &input) && is_inbounds(curr_pos, &input) && is_curr_line(pos, curr_pos, line_len)),
-        Box::new(|pos, curr_pos| is_inbounds(pos, &input) && is_inbounds(curr_pos, &input) && is_curr_line(pos, curr_pos, line_len)),
-        Box::new(|pos, curr_pos| is_inbounds(pos, &input) && is_inbounds(curr_pos, &input) && is_prev_line(pos, curr_pos, line_len)),
-        Box::new(|pos, curr_pos| is_inbounds(pos, &input) && is_inbounds(curr_pos, &input) && is_prev_line(pos, curr_pos, line_len)),
-        Box::new(|pos, curr_pos| is_inbounds(pos, &input) && is_inbounds(curr_pos, &input) && is_prev_line(pos, curr_pos, line_len)),
-        Box::new(|pos, curr_pos| is_inbounds(pos, &input) && is_inbounds(curr_pos, &input) && is_next_line(pos, curr_pos, line_len)),
-        Box::new(|pos, curr_pos| is_inbounds(pos, &input) && is_inbounds(curr_pos, &input) && is_next_line(pos, curr_pos, line_len)),
-        Box::new(|pos, curr_pos| is_inbounds(pos, &input) && is_inbounds(curr_pos, &input) && is_next_line(pos, curr_pos, line_len))
+    let get_char: Vec<Box<dyn Fn(i32, i32) -> char>> = vec![
+        Box::new(|pos, distance| _get_char(pos + offsets[0] * distance, pos + offsets[0] * (distance - 1), 0, &input, line_len)),
+        Box::new(|pos, distance| _get_char(pos + offsets[1] * distance, pos + offsets[1] * (distance - 1), 0, &input, line_len)),
+        Box::new(|pos, distance| _get_char(pos + offsets[2] * distance, pos + offsets[2] * (distance - 1), -1, &input, line_len)),
+        Box::new(|pos, distance| _get_char(pos + offsets[3] * distance, pos + offsets[3] * (distance - 1), -1, &input, line_len)),
+        Box::new(|pos, distance| _get_char(pos + offsets[4] * distance, pos + offsets[4] * (distance - 1), -1, &input, line_len)),
+        Box::new(|pos, distance| _get_char(pos + offsets[5] * distance, pos + offsets[5] * (distance - 1), 1, &input, line_len)),
+        Box::new(|pos, distance| _get_char(pos + offsets[6] * distance, pos + offsets[6] * (distance - 1), 1, &input, line_len)),
+        Box::new(|pos, distance| _get_char(pos + offsets[7] * distance, pos + offsets[7] * (distance - 1), 1, &input, line_len)),
     ];
     let mut xmas_count = 0;
     let mut i = 0;
@@ -113,17 +98,11 @@ pub fn solve_part2(in_lines: &str) -> i32 {
             continue;
         }
         let is_xmas_down =
-            tests[2](i + offsets[2], i) && get_char(&input, i + offsets[2]) == 'M' &&
-            tests[5](i + offsets[5], i) && get_char(&input, i + offsets[5]) == 'S'
-            ||
-            tests[5](i + offsets[5], i) && get_char(&input, i + offsets[5]) == 'M' &&
-            tests[2](i + offsets[2], i) && get_char(&input, i + offsets[2]) == 'S';
+            get_char[2](i, 1) == 'M' && get_char[5](i, 1) == 'S' ||
+            get_char[5](i, 1) == 'M' && get_char[2](i, 1) == 'S';
         let is_xmas_up =
-            tests[4](i + offsets[4], i) && get_char(&input, i + offsets[4]) == 'M' &&
-            tests[7](i + offsets[7], i) && get_char(&input, i + offsets[7]) == 'S'
-            ||
-            tests[7](i + offsets[7], i) && get_char(&input, i + offsets[7]) == 'M' &&
-            tests[4](i + offsets[4], i) && get_char(&input, i + offsets[4]) == 'S';
+            get_char[4](i, 1) == 'M' && get_char[7](i, 1) == 'S' ||
+            get_char[7](i, 1) == 'M' && get_char[4](i, 1) == 'S';
         xmas_count += (is_xmas_down && is_xmas_up) as i32;
         i += 1;
     }
