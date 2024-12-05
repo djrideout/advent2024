d2a2e321e06d98d649bbec4fe5d4307660b8cfb4
D.J. Rideout <djrideoutgit@proton.me>
2024-12-05 00:19:53 +0000

    Day 4 simplify the closure params and rearrange the closures in the vec

--- src/day4.rs
+++ src/day4.rs
@@ -1,6 +1,7 @@
 use regex::Regex;
+use std::sync::Arc;
 
-fn _get_char(pos: i32, curr_pos: i32, line_offset: i32, input: &str, line_len: i32) -> char {
+fn _get_char(pos: i32, curr_pos: i32, line_offset: i32, input: Arc<String>, line_len: i32) -> char {
     if pos <= 0 || pos >= input.len() as i32 || curr_pos <= 0 || curr_pos >= input.len() as i32 {
         return '.';
     }
@@ -13,16 +14,20 @@ fn _get_char(pos: i32, curr_pos: i32, line_offset: i32, input: &str, line_len: i
     '.'
 }
 
-fn get_closures() -> Vec<Box<dyn Fn(&str, i32, i32, i32) -> char>> {
-    let get_char: Vec<Box<dyn Fn(&str, i32, i32, i32) -> char>> = vec![
-        Box::new(move |input, line_len, pos, distance| _get_char(pos + (-1) * distance, pos + (-1) * (distance - 1), 0, input, line_len)),                        // Left
-        Box::new(move |input, line_len, pos, distance| _get_char(pos + (1) * distance, pos + (1) * (distance - 1), 0, input, line_len)),                          // Right
-        Box::new(move |input, line_len, pos, distance| _get_char(pos + (-line_len - 1) * distance, pos + (-line_len - 1) * (distance - 1), -1, input, line_len)), // Up-left
-        Box::new(move |input, line_len, pos, distance| _get_char(pos + (-line_len) * distance, pos + (-line_len) * (distance - 1), -1, input, line_len)),         // Up
-        Box::new(move |input, line_len, pos, distance| _get_char(pos + (-line_len + 1) * distance, pos + (-line_len + 1) * (distance - 1), -1, input, line_len)), // Up-right
-        Box::new(move |input, line_len, pos, distance| _get_char(pos + (line_len + 1) * distance, pos + (line_len + 1) * (distance - 1), 1, input, line_len)),    // Down-right
-        Box::new(move |input, line_len, pos, distance| _get_char(pos + (line_len) * distance, pos + (line_len) * (distance - 1), 1, input, line_len)),            // Down
-        Box::new(move |input, line_len, pos, distance| _get_char(pos + (line_len - 1) * distance, pos + (line_len - 1) * (distance - 1), 1, input, line_len)),    // Down-left
+fn get_closures(input: Arc<String>, line_len: i32) -> Vec<Box<dyn Fn(i32, i32) -> char>> {
+    let create_closure = |char_offset: i32, line_offset: i32| {
+        let input = input.clone();
+        Box::new(move |pos, distance| _get_char(pos + char_offset * distance, pos + char_offset * (distance - 1), line_offset, input.clone(), line_len))
+    };
+    let get_char: Vec<Box<dyn Fn(i32, i32) -> char>> = vec![
+        create_closure(-line_len - 1, -1), // Up-left
+        create_closure(-line_len, -1),     // Up
+        create_closure(-line_len + 1, -1), // Up-right
+        create_closure(-1, 0),             // Left
+        create_closure(1, 0),              // Right
+        create_closure(line_len - 1, 1),   // Down-left
+        create_closure(line_len, 1),       // Down
+        create_closure(line_len + 1, 1),   // Down-right
     ];
     return get_char;
 }
@@ -45,7 +50,7 @@ pub fn input_generator(in_lines: &str) -> Input {
 pub fn solve_part1(input_data: &Input) -> i32 {
     let line_len = input_data.line_len;
     let input = &input_data.input;
-    let get_char = get_closures();
+    let get_char = get_closures(Arc::new(input.clone()), line_len);
     let mut xmas_count = 0;
     let mut i = 0;
     for char in input.chars() {
@@ -55,9 +60,9 @@ pub fn solve_part1(input_data: &Input) -> i32 {
         }
         for j in 0 .. 8 {
             let is_xmas =
-                get_char[j](input, line_len, i, 1) == 'M' &&
-                get_char[j](input, line_len, i, 2) == 'A' &&
-                get_char[j](input, line_len, i, 3) == 'S';
+                get_char[j](i, 1) == 'M' &&
+                get_char[j](i, 2) == 'A' &&
+                get_char[j](i, 3) == 'S';
             xmas_count += is_xmas as i32;
         }
         i += 1;
@@ -69,7 +74,7 @@ pub fn solve_part1(input_data: &Input) -> i32 {
 pub fn solve_part2(input_data: &Input) -> i32 {
     let line_len = input_data.line_len;
     let input = &input_data.input;
-    let get_char = get_closures();
+    let get_char = get_closures(Arc::new(input.clone()), line_len);
     let mut xmas_count = 0;
     let mut i = 0;
     for char in input.chars() {
@@ -78,11 +83,11 @@ pub fn solve_part2(input_data: &Input) -> i32 {
             continue;
         }
         let is_xmas_down =
-            get_char[2](input, line_len, i, 1) == 'M' && get_char[5](input, line_len, i, 1) == 'S' ||
-            get_char[5](input, line_len, i, 1) == 'M' && get_char[2](input, line_len, i, 1) == 'S';
+            get_char[0](i, 1) == 'M' && get_char[7](i, 1) == 'S' ||
+            get_char[7](i, 1) == 'M' && get_char[0](i, 1) == 'S';
         let is_xmas_up =
-            get_char[4](input, line_len, i, 1) == 'M' && get_char[7](input, line_len, i, 1) == 'S' ||
-            get_char[7](input, line_len, i, 1) == 'M' && get_char[4](input, line_len, i, 1) == 'S';
+            get_char[5](i, 1) == 'M' && get_char[2](i, 1) == 'S' ||
+            get_char[2](i, 1) == 'M' && get_char[5](i, 1) == 'S';
         xmas_count += (is_xmas_down && is_xmas_up) as i32;
         i += 1;
     }
