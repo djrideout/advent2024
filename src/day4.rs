683f175ef1650ab3431c69074387ff462c9ba349
D.J. Rideout <djrideoutgit@proton.me>
2024-12-06 22:39:25 +0000

    Day 6, refactor grid utils, part 2 could be a bit more optimized

--- src/day4.rs
+++ src/day4.rs
@@ -1,49 +1,9 @@
-use regex::Regex;
 use std::sync::Arc;
-
-fn _get_char(pos: i32, curr_pos: i32, line_offset: i32, input: Arc<String>, line_len: i32) -> char {
-    if pos <= 0 || pos >= input.len() as i32 || curr_pos <= 0 || curr_pos >= input.len() as i32 {
-        return '.';
-    }
-    let curr_row = curr_pos / line_len;
-    let other_row_start = (curr_row + line_offset) * line_len;
-    let other_row_end = other_row_start + line_len - 1;
-    if pos >= other_row_start && pos <= other_row_end {
-        return input.chars().nth(pos as usize).unwrap();
-    }
-    '.'
-}
-
-fn get_closures(input: Arc<String>, line_len: i32) -> Vec<Box<dyn Fn(i32, i32) -> char>> {
-    let create_closure = |char_offset: i32, line_offset: i32| {
-        let input = input.clone();
-        Box::new(move |pos, distance| _get_char(pos + char_offset * distance, pos + char_offset * (distance - 1), line_offset, input.clone(), line_len))
-    };
-    let get_char: Vec<Box<dyn Fn(i32, i32) -> char>> = vec![
-        create_closure(-line_len - 1, -1), // Up-left
-        create_closure(-line_len, -1),     // Up
-        create_closure(-line_len + 1, -1), // Up-right
-        create_closure(-1, 0),             // Left
-        create_closure(1, 0),              // Right
-        create_closure(line_len - 1, 1),   // Down-left
-        create_closure(line_len, 1),       // Down
-        create_closure(line_len + 1, 1),   // Down-right
-    ];
-    return get_char;
-}
-
-pub struct Input {
-    input: String,
-    line_len: i32
-}
+use crate::grid::{Input, get_closures, generate_input};
 
 #[aoc_generator(day4)]
 pub fn input_generator(in_lines: &str) -> Input {
-    let new_line_regex = Regex::new(r"\n").unwrap();
-    Input {
-        input: new_line_regex.replace_all(in_lines, "").to_string(),
-        line_len: new_line_regex.find(in_lines).unwrap().start() as i32
-    }
+    generate_input(in_lines)
 }
 
 #[aoc(day4, part1)]
@@ -60,9 +20,9 @@ pub fn solve_part1(input_data: &Input) -> i32 {
         }
         for j in 0 .. 8 {
             let is_xmas =
-                get_char[j](i, 1) == 'M' &&
-                get_char[j](i, 2) == 'A' &&
-                get_char[j](i, 3) == 'S';
+                get_char[j](i, 1).0 == 'M' &&
+                get_char[j](i, 2).0 == 'A' &&
+                get_char[j](i, 3).0 == 'S';
             xmas_count += is_xmas as i32;
         }
         i += 1;
@@ -83,11 +43,11 @@ pub fn solve_part2(input_data: &Input) -> i32 {
             continue;
         }
         let is_xmas_down =
-            get_char[0](i, 1) == 'M' && get_char[7](i, 1) == 'S' ||
-            get_char[7](i, 1) == 'M' && get_char[0](i, 1) == 'S';
+            get_char[0](i, 1).0 == 'M' && get_char[7](i, 1).0 == 'S' ||
+            get_char[7](i, 1).0 == 'M' && get_char[0](i, 1).0 == 'S';
         let is_xmas_up =
-            get_char[5](i, 1) == 'M' && get_char[2](i, 1) == 'S' ||
-            get_char[2](i, 1) == 'M' && get_char[5](i, 1) == 'S';
+            get_char[5](i, 1).0 == 'M' && get_char[2](i, 1).0 == 'S' ||
+            get_char[2](i, 1).0 == 'M' && get_char[5](i, 1).0 == 'S';
         xmas_count += (is_xmas_down && is_xmas_up) as i32;
         i += 1;
     }
---
+++ src/day6.rs
@@ -0,0 +1,85 @@
+use regex::Regex;
+use std::sync::Arc;
+use crate::grid::{Input, get_closures, generate_input};
+
+#[aoc_generator(day6)]
+pub fn input_generator(in_lines: &str) -> Input {
+    generate_input(in_lines)
+}
+
+fn follow_path(input: &String, line_len: i32, start_pos: i32, start_direction: usize, block_pos: i32, mut on_pos: impl FnMut(char, i32, usize) -> bool) -> bool {
+    let get_char = get_closures(Arc::new(input.clone()), line_len);
+    let mut pos = start_pos;
+    let mut direction = start_direction;
+    loop {
+        let (next, i) = get_char[direction](pos, 1);
+        let prev_direction = direction;
+        if next == 'o' {
+            break;
+        } else if next == '#' || i == block_pos {
+            direction = match direction {
+                1 => 4,
+                4 => 6,
+                6 => 3,
+                3 => 1,
+                _ => 1
+            }
+        } else {
+            pos = i;
+        }
+        if on_pos(next, i, prev_direction) {
+            return true;
+        }
+    }
+    false
+}
+
+#[aoc(day6, part1)]
+pub fn solve_part1(input_data: &Input) -> i32 {
+    let line_len = input_data.line_len;
+    let input = &input_data.input;
+    let pos = Regex::new(r"\^").unwrap().find(input).unwrap().start() as i32;
+    let mut positions = vec![pos];
+    follow_path(input, line_len, pos, 1, -1, |next, i, _| {
+        if next != '#' && !positions.contains(&i) {
+            positions.push(i);
+        }
+        false
+    });
+    positions.len() as i32
+}
+
+struct Collision {
+    pos: i32,
+    direction: usize
+}
+
+#[aoc(day6, part2)]
+pub fn solve_part2(input_data: &Input) -> i32 {
+    let line_len = input_data.line_len;
+    let input = &input_data.input;
+    let pos = Regex::new(r"\^").unwrap().find(input).unwrap().start() as i32;
+    let mut positions = vec![];
+    follow_path(input, line_len, pos, 1, -1, |next, i, _| {
+        if next != '#' && i != pos {
+            let mut collisions = vec![];
+            let will_loop = follow_path(input, line_len, pos, 1, i, |test_next, test_i, test_dir| {
+                if test_next == '#' || test_i == i {
+                    if collisions.iter().any(|c: &Collision| c.pos == test_i && c.direction == test_dir) {
+                        return true;
+                    }
+                    collisions.push(Collision {
+                        pos: test_i,
+                        direction: test_dir
+                    });
+                }
+                false
+            });
+            if will_loop && !positions.contains(&i) {
+                positions.push(i);
+            }
+        }
+        false
+    });
+    positions.len() as i32
+}
