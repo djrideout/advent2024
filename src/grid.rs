36c8165b87596d7f66a04df118ffe042cf6dee9d
D.J. Rideout <djrideoutgit@proton.me>
2024-12-10 23:00:19 +0000

    Change grid utils to a normal struct, simplifies things a bit

--- src/grid.rs
+++ src/grid.rs
@@ -1,43 +1,4 @@
 use regex::Regex;
-use std::sync::Arc;
-
-pub fn is_inbounds(pos: i32, curr_pos: i32, line_offset: i32, input_len: usize, line_len: i32) -> bool {
-    if pos < 0 || pos >= input_len as i32 || curr_pos < 0 || curr_pos >= input_len as i32 {
-        return false;
-    }
-    let curr_row = curr_pos / line_len;
-    let other_row_start = (curr_row + line_offset) * line_len;
-    let other_row_end = other_row_start + line_len - 1;
-    if pos >= other_row_start && pos <= other_row_end {
-        return true;
-    }
-    false
-}
-
-fn _get_char(pos: i32, curr_pos: i32, line_offset: i32, input: Arc<String>, line_len: i32) -> char {
-    if is_inbounds(pos, curr_pos, line_offset, input.len(), line_len) {
-        return input.chars().nth(pos as usize).unwrap();
-    }
-    '!'
-}
-
-pub fn get_closures(input: Arc<String>, line_len: i32) -> Vec<Box<dyn Fn(i32, i32) -> (char, i32)>> {
-    let create_closure = |char_offset: i32, line_offset: i32| {
-        let input = input.clone();
-        Box::new(move |pos, distance| (_get_char(pos + char_offset * distance, pos + char_offset * (distance - 1), line_offset, input.clone(), line_len), pos + char_offset * distance))
-    };
-    let get_char: Vec<Box<dyn Fn(i32, i32) -> (char, i32)>> = vec![
-        create_closure(-line_len - 1, -1), // Up-left
-        create_closure(-line_len, -1),     // Up
-        create_closure(-line_len + 1, -1), // Up-right
-        create_closure(-1, 0),             // Left
-        create_closure(1, 0),              // Right
-        create_closure(line_len - 1, 1),   // Down-left
-        create_closure(line_len, 1),       // Down
-        create_closure(line_len + 1, 1),   // Down-right
-    ];
-    return get_char;
-}
 
 pub struct Input {
     pub input: String,
@@ -49,5 +10,51 @@ pub fn generate_input(in_lines: &str) -> Input {
     Input {
         input: new_line_regex.replace_all(in_lines, "").to_string(),
         line_len: new_line_regex.find(in_lines).unwrap().start() as i32
+    }
+}
+
+pub struct Grid {
+    data: String,
+    line_len: i32
+}
+
+impl Grid {
+    pub fn new(data: String, line_len: i32) -> Grid {
+        Grid {
+            data,
+            line_len
+        }
+    }
+
+    pub fn is_inbounds(&self, pos: i32, curr_pos: i32, line_offset: i32) -> bool {
+        if pos < 0 || pos >= self.data.len() as i32 || curr_pos < 0 || curr_pos >= self.data.len() as i32 {
+            return false;
+        }
+        let curr_row = curr_pos / self.line_len;
+        let other_row_start = (curr_row + line_offset) * self.line_len;
+        let other_row_end = other_row_start + self.line_len - 1;
+        if pos >= other_row_start && pos <= other_row_end {
+            return true;
+        }
+        false
+    }
+
+    pub fn get_char(&self, pos: i32, direction: usize, distance: i32) -> (char, i32) {
+        let (char_offset, line_offset) = match direction {
+            0 => (-self.line_len - 1, -1), // Up-left
+            1 => (-self.line_len, -1),     // Up
+            2 => (-self.line_len + 1, -1), // Up-right
+            3 => (-1, 0),                  // Left
+            4 => (1, 0),                   // Right
+            5 => (self.line_len - 1, 1),   // Down-left
+            6 => (self.line_len, 1),       // Down
+            7 => (self.line_len + 1, 1),   // Down-right
+            _ => (0, 0)                    // Oh no
+        };
+        let char_pos = pos + char_offset * distance;
+        if self.is_inbounds(char_pos, pos + char_offset * (distance - 1), line_offset) {
+            return (self.data.chars().nth(char_pos as usize).unwrap(), char_pos);
+        }
+        ('!', char_pos)
     }
 }
