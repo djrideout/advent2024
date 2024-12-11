3deec589054c545c83ae35a48a97049b9e2862c0
D.J. Rideout <djrideoutgit@proton.me>
2024-12-11 21:56:51 +0000

    Day 11, much better

--- src/day11.rs
+++ src/day11.rs
@@ -6,56 +6,31 @@ pub fn input_generator(input: &str) -> Vec<u64> {
 }
 
 fn count_stones(input: &Vec<u64>, blinks: i32) -> usize {
-    let mut child_graph = HashMap::<u64, Vec<u64>>::new();
-    let mut appearances = HashMap::<u64, usize>::new();
-    let mut to_compute_children = input.clone();
-    to_compute_children.iter().for_each(|t| {
-        appearances.insert(*t, 1);
+    let mut counts = HashMap::<u64, usize>::new();
+    input.iter().for_each(|stone_num| {
+        counts.insert(*stone_num, 1);
     });
     for _ in 0 .. blinks {
-        let mut next_to_compute_children: Vec<u64> = vec![];
-        while let Some(stone) = to_compute_children.pop() {
-            let mut stone_str = stone.to_string();
+        let mut next_counts = HashMap::<u64, usize>::new();
+        for (stone_num, count) in counts {
+            let mut stone_str = stone_num.to_string();
             let mut children: Vec<u64> = vec![];
-            if stone == 0 {
+            if stone_num == 0 {
                 children.push(1);
             } else if stone_str.len() % 2 == 0 {
                 children.push(stone_str.split_off(stone_str.len() / 2).parse().unwrap());
                 children.push(stone_str.parse().unwrap());
             } else {
-                children.push(stone * 2024);
+               children.push(stone_num * 2024);
             }
-            for i in 0 .. children.len() {
-                if !child_graph.contains_key(&children[i]) {
-                    next_to_compute_children.push(children[i]);
-                }
-            }
-            child_graph.insert(stone, children);
-        }
-        to_compute_children = next_to_compute_children;
-        let mut next_appearances = appearances.clone();
-        let mut adds =  HashMap::<u64, usize>::new();
-        let mut removes =  HashMap::<u64, usize>::new();
-        for (key, value) in appearances.into_iter() {
-            if value > 0 {
-                removes.insert(key, value);
-                let children = child_graph.get(&key).unwrap();
-                for i in 0 .. children.len() {
-                    let child_count = adds.get(&children[i]);
-                    adds.insert(children[i], child_count.or(Some(&0)).unwrap() + value);
-                }
+            for child in children {
+                let child_count = next_counts.get(&child).unwrap_or(&0);
+                next_counts.insert(child, child_count + count);
             }
         }
-        for (remove, _) in removes {
-            next_appearances.insert(remove, 0);
-        }
-        for (add, count) in adds {
-            let value = next_appearances.get(&add);
-            next_appearances.insert(add, value.or(Some(&0)).unwrap() + count);
-        }
-        appearances = next_appearances;
+        counts = next_counts;
     }
-    appearances.into_iter().fold(0, |accu, (_, b)| accu + b)
+    counts.iter().fold(0, |accu, (_, count)| accu + count)
 }
 
 #[aoc(day11, part1)]
