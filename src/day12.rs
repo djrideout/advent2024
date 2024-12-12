96deb9b06c677e32257b1895235249e3e5658b47
D.J. Rideout <djrideoutgit@proton.me>
2024-12-12 21:51:30 +0000

    Day 12, part 2

--- src/day12.rs
+++ src/day12.rs
@@ -5,9 +5,53 @@ pub fn input_generator(in_lines: &str) -> Input {
     generate_input(in_lines)
 }
 
-struct Area {
+struct FenceSegment {
+    direction: usize,
+    positions: Vec<i32>
+}
+
+struct Area<'a> {
+    grid: &'a Grid,
     positions: Vec<i32>,
-    perimeter: usize
+    segments: Vec<FenceSegment>
+}
+
+impl Area<'_> {
+    pub fn add_fence(&mut self, pos: i32, direction: usize) {
+        let test_directions: Vec<usize> = match direction {
+            1 => vec![3, 4],
+            6 => vec![3, 4],
+            3 => vec![1, 6],
+            4 => vec![1, 6],
+            _ => vec![]
+        };
+        let mut segments: Vec<usize> = vec![];
+        for test_direction in test_directions {
+            let (_, next_pos) = self.grid.get_char(pos, test_direction, 1);
+            let segment_pos = self.segments.iter().position(|seg| seg.direction == direction && seg.positions.contains(&next_pos));
+            if segment_pos.is_some() {
+                segments.push(segment_pos.unwrap());
+            }
+        }
+        let mut new_segment = FenceSegment {
+            direction,
+            positions: vec![pos]
+        };
+        segments.sort_by(|a, b| b.cmp(a));
+        for old_pos in segments {
+            new_segment.positions.append(&mut self.segments[old_pos].positions);
+            self.segments.remove(old_pos);
+        }
+        self.segments.push(new_segment);
+    }
+
+    pub fn count(&self) -> usize {
+        self.segments.len()
+    }
+
+    pub fn perimeter(&self) -> usize {
+        self.segments.iter().map(|segment| segment.positions.len()).sum()
+    }
 }
 
 fn compute_area(pos: i32, char: char, grid: &Grid, seen: &mut Vec<i32>, area: &mut Area) {
@@ -17,7 +61,7 @@ fn compute_area(pos: i32, char: char, grid: &Grid, seen: &mut Vec<i32>, area: &m
     for i in directions {
         let (next_char, next_pos) = grid.get_char(pos, i, 1);
         if next_char != char {
-            area.perimeter += 1;
+            area.add_fence(pos, i);
             continue;
         }
         if area.positions.contains(&next_pos) {
@@ -27,7 +71,7 @@ fn compute_area(pos: i32, char: char, grid: &Grid, seen: &mut Vec<i32>, area: &m
     }
 }
 
-fn count(input_data: &Input) -> usize {
+fn count(input_data: &Input, discount: bool) -> usize {
     let Input { input, line_len } = input_data;
     let grid = Grid::new(input.clone(), *line_len);
     let mut total = 0;
@@ -37,16 +81,26 @@ fn count(input_data: &Input) -> usize {
             continue;
         }
         let mut area = Area {
+            grid: &grid,
             positions: vec![],
-            perimeter: 0
+            segments: vec![]
         };
         compute_area(i as i32, char, &grid, &mut seen, &mut area);
-        total += area.positions.len() * area.perimeter;
+        if discount {
+            total += area.positions.len() * area.count();
+        } else {
+            total += area.positions.len() * area.perimeter();
+        }
     }
     total
 }
 
 #[aoc(day12, part1)]
 pub fn solve_part1(input_data: &Input) -> usize {
-    count(input_data)
+    count(input_data, false)
+}
+
+#[aoc(day12, part2)]
+pub fn solve_part2(input_data: &Input) -> usize {
+    count(input_data, true)
 }
