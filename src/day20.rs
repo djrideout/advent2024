75fcb3f2a1029683b064d8c8e347e20dd4e31bd1
D.J. Rideout <djrideoutgit@proton.me>
2024-12-21 21:55:50 +0000

    Day 20, part 2, maybe I'll keep cleaning this up some time but really tired of it

--- src/day20.rs
+++ src/day20.rs
@@ -1,3 +1,4 @@
+use std::collections::HashMap;
 use regex::Regex;
 use crate::grid::{generate_input, Grid, Input, get_opposite_direction};
 
@@ -31,58 +32,66 @@ fn follow_path(grid: &Grid, start_position: i32, end_positions: &Vec<(i32, usize
     }
 }
 
-fn print_map(map: &String, path: &Vec<(i32, usize)>, line_len: i32) {
-    map.chars().enumerate().for_each(|(i, c)| {
-        if i as i32 % line_len == 0 {
-            println!();
+fn get_possible_cheats(pos: i32, cheat_length: i32, line_len: i32, grid: &Grid) -> Vec<(i32, i32)> {
+    let step_count = cheat_length - 1;
+    let mut positions = vec![];
+    let x0 = pos % line_len;
+    let y0 = pos / line_len;
+    for x in x0 - step_count .. x0 + step_count + 1 {
+        if x < 0 || x >= line_len {
+            continue;
         }
-        if path.iter().any(|(path_pos, _)| i == (*path_pos) as usize) {
-            print!("@");
-        } else {
-            print!("{c}");
+        for y in y0 - step_count .. y0 + step_count + 1 {
+            let linear = x + line_len * y;
+            let (char, _) = grid.get_char(linear, 0, 0);
+            if char != '#' && char != '!' && (x - x0).abs() + (y - y0).abs() < cheat_length {
+                positions.push((linear, (x - x0).abs() + (y - y0).abs()));
+            }
         }
-    });
-    println!();
+    }
+    positions
 }
 
-#[aoc(day20, part1)]
-pub fn solve_part1(input: &Input) -> i32 {
+fn count_cheats(input: &Input, cheat_length: i32) -> i32 {
     let start_pos = Regex::new(r"S").unwrap().find(&input.input).unwrap().start() as i32;
     let end_pos = Regex::new(r"E").unwrap().find(&input.input).unwrap().start() as i32;
     let grid = Grid::new(input.input.clone(), input.line_len);
     let path = follow_path(&grid, start_pos, &vec![(end_pos, 0)], 0);
     let mut count = 0;
     for (i, (pos, _)) in path.iter().enumerate() {
+        let mut cheats = HashMap::new();
         for test_direction in [1, 3, 4, 6] {
-            let (test_char, test_pos) = grid.get_char(*pos, test_direction, 1);
-            if test_char != '#' {
-                continue;
-            }
-            for next_direction in [1, 3, 4, 6] {
-                let mut test_map = input.input.clone();
-                if next_direction == get_opposite_direction(test_direction) {
-                    continue;
-                }
-                test_map.replace_range((test_pos as usize) .. (test_pos as usize) + 1, ".");
-                let (next_char, next_pos) = grid.get_char(test_pos, next_direction, 1);
-                if next_char == '#' || next_char == '!' {
-                    continue;
-                }
-                test_map.replace_range((next_pos as usize) .. (next_pos as usize) + 1, ".");
-                let test_grid = Grid::new(test_map.clone(), input.line_len);
-                let test_path = follow_path(&test_grid, test_pos, &path, next_direction);
-                if test_path.len() == 0 {
-                    continue;
-                }
-                let last_test_pos = test_path.last().unwrap().0;
-                let old_distance = path.iter().position(|(pos, _)| *pos == last_test_pos).unwrap() - i;
-                let new_distance = test_path.len();
-                let diff = new_distance as i32 - old_distance as i32;
-                if diff <= -100 {
-                    count += 1;
+            let (_, test_pos) = grid.get_char(*pos, test_direction, 1);
+            let positions = get_possible_cheats(test_pos, cheat_length, input.line_len, &grid);
+            for (end_pos, steps) in &positions {
+                let old_dist_to_end = (path.len() - i) as i32;
+                let new_dist_to_end = steps + (path.len() - path.iter().position(|(pos, _)| *pos == *end_pos).unwrap()) as i32;
+                let diff = new_dist_to_end - old_dist_to_end;
+                if !cheats.contains_key(&end_pos.clone()) {
+                    cheats.insert(end_pos.clone(), diff);
+                } else {
+                    let old = cheats.get(&end_pos.clone()).unwrap();
+                    if diff <= *old {
+                        cheats.insert(end_pos.clone(), diff);
+                    }
                 }
             }
         }
+        for (_, diff) in cheats {
+            if diff <= -100 {
+                count += 1;
+            }
+        }
     }
-    count
+    count as i32
+}
+
+#[aoc(day20, part1)]
+pub fn solve_part1(input: &Input) -> i32 {
+    count_cheats(input, 2)
+}
+
+#[aoc(day20, part2)]
+pub fn solve_part2(input: &Input) -> i32 {
+    count_cheats(input, 20)
 }
